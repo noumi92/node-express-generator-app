f8e2f5a328bd32a206a1c534099b69ef20fbfe60
noumi92 <noumankarim1988@gmail.com>
2022-03-27 17:10:02 +0500

    models added

---
+++ models/dishes.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+const dishSchema = new Schema(
+    {
+    name:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    comments: [commentSchema]
+},{
+    timestamps: true
+});
+
+var dishes = mongoose.model('Dish', dishSchema);
+module.exports = dishes;
