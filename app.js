e60db5c7d1ac48940b77667aa1bda1df6240a1f8
noumi92 <noumankarim1988@gmail.com>
2022-03-31 16:20:18 +0500

    CookiesParser added

--- app.js
+++ app.js
@@ -1,83 +1,98 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var mongoose = require('mongoose');
-var dotenv = require('dotenv');
-var Dishes = require('./models/dishes');
-var Promotions = require('./models/promotions');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var dotenv = require("dotenv");
+var Dishes = require("./models/dishes");
+var Promotions = require("./models/promotions");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// middleware uses
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 dotenv.config();
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser("12345-67890-09876-54321"));
 
 //authentication
-function auth(req, res, next){
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if(!authHeader){
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
-    return;
-  }
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64')
-  .toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
-  if (username === 'admin' && password === 'password'){
-    next();
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+      return;
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
+    var username = auth[0];
+    var password = auth[1];
+    if (username === "admin" && password === "password") {
+      res.cookie('user','admin',{signed: true});
+      next();
+    } else {
+      var err = new Error("You are not authenticated!");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      next(err);
+    }
   }else{
-    var err = new Error('You are not authenticated!');
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-    next(err);
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }else{
+      var err = new Error("You are not authenticated!");
+      err.status = 401;
+      next(err);
+    }
   }
 }
 app.use(auth);
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/leaders', leaderRouter);
-app.use('/promotions', promoRouter);
-//database 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/leaders", leaderRouter);
+app.use("/promotions", promoRouter);
+//database
 const connect = mongoose.connect(process.env.DATABASE_URL);
-connect.then((db)=>{
-  console.log('Connected to DB Server');
-}, (err)=>{
-  console.log(err);
-});
+connect.then(
+  (db) => {
+    console.log("Connected to DB Server");
+  },
+  (err) => {
+    console.log(err);
+  }
+);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
